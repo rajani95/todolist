5747859ac5960d8b300c7f38a045f2168440a431
rajani95 <31245757+rajani95@users.noreply.github.com>
2017-09-19 17:45:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+var express	= require('express');
+var app		= express();
+var mongoose	= require('mongoose');
+var port	= process.env.PORT || 8080;
+var database	= require('./config/database');
+
+var morgan	= require('morgan');
+var bodyParser	= require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('database.url');
+
+app.use(express.static(__dirname + '/public')); 				// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); 										// log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'})); 			// parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); 									// parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride());
+
+var Todo = mongoose.model('Todo', {
+	text: String
+});
+
+app.get('/api/todos', function(req, res) {
+
+        // use mongoose to get all todos in the database
+        Todo.find(function(err, todos) {
+
+            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+            if (err)
+                res.send(err)
+
+            res.json(todos); // return all todos in JSON format
+        });
+    });
+
+    // create todo and send back all todos after creation
+    app.post('/api/todos', function(req, res) {
+
+        // create a todo, information comes from AJAX request from Angular
+        Todo.create({
+            text : req.body.text,
+            done : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+
+    });
+
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Todo.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+    });
+
+app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+
+app.listen(port);
+console.log("App listening on port " + port);	
